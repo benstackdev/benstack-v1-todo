7a84252392bb8509a8f6d30f81b99d277c2faf5e
itmanbanana <ben@benstack.dev>
2026-07-13 20:12:18 -0400

    db: delete all expired sessions from user

--- packages/db/src/queries/index.ts
+++ packages/db/src/queries/index.ts
@@ -1,12 +1,16 @@
 // Naming convention: actionDescription
+// TODO: Clean up exports
 
 import { insertUser, selectUserByEmail } from "./users-queries.js";
-import { insertSession, selectSessionByToken, deleteAllUserSessions } from "./sessions-queries.js";
+import { insertSession, selectSessionByToken, deleteAllUserSessions, deleteSessionById, selectAllUserSessions, deleteExpiredUserSessions } from "./sessions-queries.js";
 
 export {
   insertSession,
   insertUser,
   selectUserByEmail,
   selectSessionByToken,
-  deleteAllUserSessions
+  selectAllUserSessions,
+  deleteSessionById,
+  deleteAllUserSessions,
+  deleteExpiredUserSessions
 };
--- packages/db/src/queries/sessions-queries.ts
+++ packages/db/src/queries/sessions-queries.ts
@@ -1,6 +1,6 @@
 import { db } from "../../client.js";
 import { session } from "../schema/sessions.js";
-import { eq } from "drizzle-orm";
+import { and, eq, lt } from "drizzle-orm";
 
 type SessionType = typeof session.$inferInsert;
 
@@ -28,11 +28,54 @@ export const selectSessionByToken = async (token: SessionType["token"]) => {
   }
 };
 
+export const selectAllUserSessions = async (userId: SessionType["userId"]) => {
+  try {
+    const userSessions = await db
+      .select().from(session)
+      .where(eq(session.userId, userId));
+
+    return userSessions;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteSessionById = async (id: SessionType["id"]) => {
+  try {
+    if (!id) return;
+
+    const deletedSession = await db.delete(session)
+      .where(eq(session.id, id)).returning();
+
+    return deletedSession;
+
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteExpiredUserSessions = async (userId: SessionType["userId"], date: Date) => {
+  try {
+    if (!userId) return;
+
+    const deletedSessions = await db.delete(session)
+      .where(and(
+        eq(session.userId, userId),
+        lt(session.expiresAt, date)
+      ));
+
+    return deletedSessions;
+  } catch (error) {
+    throw error;
+  }
+};
+
 export const deleteAllUserSessions = async (userId: SessionType["userId"]) => {
   try {
     if (!userId) return;
 
-    const deletedSessions = await db.delete(session).where(eq(session.userId, userId)).returning();
+    const deletedSessions = await db.delete(session)
+      .where(eq(session.userId, userId)).returning();
 
     return deletedSessions;
   } catch (error) {
