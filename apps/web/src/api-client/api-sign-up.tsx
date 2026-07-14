de384bc449644e6c8a797e28f5641d41e8e4c697
itmanbanana <ben@benstack.dev>
2026-07-14 17:28:15 -0400

    web: add shadcn and theme provider with vite

---
+++ apps/web/src/api-client/api-sign-up.tsx
@@ -0,0 +1,47 @@
+import * as z from "zod";
+import { User } from "shared";
+
+export const signup = async (email: string, password: string, passwordConfirm: string) => {
+  const errors: string[] = [];
+
+  // Check given passwords match
+  if (password !== passwordConfirm) errors.push("Passwords do not match");
+
+  // Validate given email and password
+  try { User.Email.parse(email); }
+  catch (error) {
+    if (error instanceof z.ZodError) {
+      errors.push("Invalid email address");
+    }
+  }
+
+  try { User.Password.parse(password); }
+  catch (error) {
+    if (error instanceof z.ZodError) {
+      errors.push("Invalid password");
+    }
+  }
+
+  // POST to api: /auth/sign-up
+  if (errors.length > 0) return errors;
+
+  try {
+    const response = await fetch(`http://localhost:8080/auth/sign-up`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    });
+
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}; 
--- apps/web/src/routes.ts
+++ apps/web/src/routes.ts
@@ -1,8 +1,18 @@
 import { createBrowserRouter } from "react-router";
 import Root from "./routes/root.tsx";
+import Home from "./routes/home.tsx";
 
 const router = createBrowserRouter([
-  { path: "/", Component: Root },
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home
+      }
+    ]
+  },
 ]);
 
 export default router;
