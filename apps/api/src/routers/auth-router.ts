50fef7a50ed9408e1761d35e791339ce9da2cfc0
itmanbanana <ben@benstack.dev>
2026-07-19 14:23:15 -0400

    api: fix /auth/verify bug
    - Could not be used as endpoint and middleware, so added
    next function to this route to execute

--- apps/api/src/routers/auth-router.ts
+++ apps/api/src/routers/auth-router.ts
@@ -1,10 +1,10 @@
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
 import { authSigninPost, authSignoutPost, authSignupPost, authVerify } from "../controllers/auth-controller.js";
 
 const auth = new Hono();
 
 auth.get('/', (c) => c.text('Auth API'));
-auth.get('/verify', authVerify);
+auth.get('/verify', authVerify, (c: Context) => c.json({ success: "ok" }));
 auth.post('/sign-up', authSignupPost);
 auth.post('/sign-in', authSigninPost);
 auth.post('/sign-out', authSignoutPost);
